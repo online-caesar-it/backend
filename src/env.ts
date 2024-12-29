6565163c4102fc81f8798b2ebc6a6f51eb10f19a
Your Name <you@example.com>
2024-12-29 18:24:33 +0500

    feat: db config and docker

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,8 @@
 import { config } from "dotenv";
+config();
 
 export const envConfig = {
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
   SECRET_KEY: process.env.SECRET_KEY,
 };
-
-config();
