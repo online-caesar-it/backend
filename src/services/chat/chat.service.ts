c73a41b013f1b32082178325ec7502e5caf09022
nolood <a9514548799@gmail.com>
2025-01-05 16:14:01 +0300

    fix: docker compose + start of chat service

---
+++ src/services/chat/chat.service.ts
@@ -0,0 +1,11 @@
+const getMyChats = () => {};
+
+const sendMessage = () => {};
+
+const getMessages = () => {};
+
+const getChatUsers = () => {};
+
+const createChat = () => {};
+
+export const chatService = () => {};
